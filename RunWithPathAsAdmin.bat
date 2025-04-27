479f9e1d01cdcea322a067fc54c5a4f3a3ad428c
vectoa <42373955+vectoa@users.noreply.github.com>
2025-04-28 01:31:25 +0200

    Update RunWithPathAsAdmin.bat

--- RunWithPathAsAdmin.bat
+++ RunWithPathAsAdmin.bat
@@ -1,3 +1,10 @@
+@echo off
+REM ------------------------------------------------------------------
+REM  InvisiShell Profiler – Copyright © 2025  vectoa
+REM  SPDX-License-Identifier: MIT
+REM  See LICENSE file at the project root for full text.
+REM ------------------------------------------------------------------
+
 set COR_ENABLE_PROFILING=1
 set COR_PROFILER={cf0d821e-299b-5307-a3d8-b283c03916db}
 set COR_PROFILER_PATH=%~dp0InShellProf.dll
